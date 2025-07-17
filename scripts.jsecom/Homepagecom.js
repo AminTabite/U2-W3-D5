be3abf450ee91a306a66112be01206f6ce15e44e
AminTabite <Aminepicode13@gmail.com>
2025-07-17 13:32:02 +0200

    .
    .

--- scripts.jsecom/Homepagecom.js
+++ scripts.jsecom/Homepagecom.js
@@ -12,35 +12,36 @@ const getBiscuits = function () {
         "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODc4YWRkNTYzZDk3MTAwMTUwZGY2MWQiLCJpYXQiOjE3NTI3MzkyODUsImV4cCI6MTc1Mzk0ODg4NX0.ksNBhJG5JWfSXzMJhRS54qyWSpL1i7vr38VYsTY0AT8",
     },
   })
-    .then((Response) => {
-      if (Response.ok) {
-        return Response.json();
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
       } else {
-        throw new Error("errore prima fetch");
+        throw new Error("Errore nella fetch");
       }
     })
     .then((arrayOfBiscuits) => {
-      console.log("array di biscotti", arrayOfBiscuits);
-    })
-    .then(
+      console.log("array di biscotti:", arrayOfBiscuits);
+
+      const row = document.getElementById("Biscuits row");
+
       arrayOfBiscuits.forEach((biscotti) => {
-        row.innerHtml += `
-            <div class="col">
-              <div class="card h-100 d-flex flex-column">
-                <img ${biscotti.} class="card-img-top" alt="biscotti">
-                <div class="card-body d-flex flex-column">
-                  <h5 class="card-title">${biscotti.name}</h5>
-                  <p class="card-text flex-grow-1">${biscotti.description}</p>
-                  <p class="card-text">${biscotti.brand} - ${biscotti.price}</p>
-                  <a href="./detail.html?eventId= id biscotto" class="btn btn-primary">Vai ai dettagli</a>
-                </div>
+        row.innerHTML += `
+          <div class="col">
+            <div class="card h-100 d-flex flex-column">
+              <img src="${biscotti.imageUrl}" class="card-img-top" alt="biscotti">
+              <div class="card-body d-flex flex-column">
+                <h5 class="card-title">${biscotti.name}</h5>
+                <p class="card-text flex-grow-1">${biscotti.description}</p>
+                <p class="card-text">${biscotti.brand} - €${biscotti.price}</p>
+                <a href="./details.html?eventId=${biscotti._id}" class="btn btn-primary">Vai ai dettagli</a>
               </div>
             </div>
-          `;
-      })
-    )
+          </div>
+        `;
+      });
+    })
     .catch((err) => {
-      console.log("errore per ora non c'é nulla", err);
+      console.log("Errore:", err);
     });
 };
 
