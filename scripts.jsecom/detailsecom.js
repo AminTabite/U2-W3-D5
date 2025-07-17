50bef5183097e1921ea2dc172923c71d57aaee6a
AminTabite <Aminepicode13@gmail.com>
2025-07-17 16:50:39 +0200

    fine
    .

--- scripts.jsecom/detailsecom.js
+++ scripts.jsecom/detailsecom.js
@@ -48,3 +48,33 @@ fetch(endpoint + "/" + biscuitId, {
    .catch((err) => {
     console.log('ERRORE', err)
   })
+
+  const deleteBiscotto = function () {
+  // con questa funzione chiedo all'API di eliminare questa risorsa
+  fetch(endpoint + "/" + biscuitId, {
+  method: "DELETE",         // metodo fuori da headers
+  headers: {
+    Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODc4YWRkNTYzZDk3MTAwMTUwZGY2MWQiLCJpYXQiOjE3NTI3MzkyODUsImV4cCI6MTc1Mzk0ODg4NX0.ksNBhJG5JWfSXzMJhRS54qyWSpL1i7vr38VYsTY0AT8"
+  }
+})
+
+.then((response) => {
+      if (response.ok) {
+        
+        alert("Rip biscotto");
+        // visto che la risorsa non esiste più, riportiamo l'utente in home
+        location.assign("/homepage.html");
+      } else {
+        throw new Error("Errore in fase di eliminazione");
+      }
+    })
+    .catch((err) => {
+      console.log("ERRORE", err);
+    });
+};
+
+const editBiscotto = function () {
+  // dobbiamo re-direzionare l'utente alla pagina backoffice con un parametro nell'URL
+  location.assign('/backoffice.html?biscuitId=' + biscuitId)
+
+}
